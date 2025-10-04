b0a3967726113f37c7d25afd1fa2207075d683f7
Saurya-0301 <sauryaanand2003@gmail.com>
2025-10-04 11:28:50 +0530

    Updated

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -14,7 +14,13 @@ export default function RootLayout({ children }) {
       <body
         className={'${inter.className}'}
       >
-        {children}
+          <main className="min-h-screen">{children}</main>
+      <footer className="bg-blue-50 py-12">
+        <div className="container mx-auto px-4 text-center text-gray-600">
+          <p>Links</p>
+        </div>
+      </footer>
+    
       </body>
     </html>
   );
