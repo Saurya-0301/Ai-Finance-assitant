dee82a1111ce4c8295809e21256fba173d0f0b45
Saurya-0301 <sauryaanand2003@gmail.com>
2025-10-05 21:46:51 +0530

    Updated

--- my-app/components/header.jsx
+++ my-app/components/header.jsx
@@ -2,6 +2,7 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "./ui/button"
+import { LayoutDashboard, PenBox } from "lucide-react"
 
 const Header = () => {
   return (
@@ -15,7 +16,23 @@ const Header = () => {
         width={200}
         className="h-12 w-auto object-contain"/>
       </Link>
-    
+    <div className="flex items-center space-x-4">
+      <SignedIn>
+        <Link href={"/dashboard"}
+        className="text-gray-600 hovertext-blue-600 flex items-center gap-2">
+        <Button variant="outline">
+          <LayoutDashboard size={18} />
+          <span className="hidden md:inline">Dashboard</span>
+        </Button>
+        </Link>
+
+         <Link href={"/transaction/create"}>
+        <Button className="flex items-center gap-2">
+          <PenBox size={18} />
+          <span className="hidden md:inline">Add transaction</span>
+        </Button>
+        </Link>
+      </SignedIn>
     
     <SignedOut>
       <SignInButton forceRedirectUrl="/dashboard">
@@ -23,8 +40,13 @@ const Header = () => {
               </SignInButton>
             </SignedOut>
             <SignedIn>
-              <UserButton />
+              <UserButton appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }} />
             </SignedIn>
+            </div>
             </nav>
             </div>
   )
