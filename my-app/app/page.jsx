262e0dfd02f856ec8528991ddce1a20994b65c6f
Saurya-0301 <sauryaanand2003@gmail.com>
2025-10-17 21:25:06 +0530

    Updated

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,7 +1,9 @@
 import HeroSection from "@/components/hero";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { featuresData, howItWorksData, statsData, testimonialsData } from "@/data/landing";
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function home() {
@@ -57,13 +59,18 @@ export default function home() {
           {testimonialsData.map((testimonial,index)=>(
             <Card key={index} className="p-6">
   <CardContent className="pt-4">
-    <div>
+    <div className="flex items-center mb-4">
       <Image src={testimonial.image}
       alt={testimonial.name}
       width={40}
       height={40}
       className="rounded-full"/>
+      <div className="ml-4">
+        <div className="font-semibold">{testimonial.name}</div>
+        <div className="text-sm text-gray-600">{testimonial.role}</div>
+      </div>
     </div>
+    <p className="text-gray-600">{testimonial.quote}</p>
     
   </CardContent>
 </Card>
@@ -71,7 +78,21 @@ export default function home() {
         </div>
       </div>
     </section>
-    
+    <section className="py-20 bg-blue-600"> 
+      <div className="container mx-auto px-4 text-center">
+        <h2 className="text-3xl font-bold text-center mb-4 text-white">
+         Ready to build a smarter financial future?
+        </h2>
+        <p className="text-blue-100 mb-8 max-w-2xl mx-auto">
+          Join thousand of users who are already managing their finances smarter with FinAi
+        </p>
+        <Link href="/dashboard">
+        <Button size="lg" className="bg-white text-blue-600 hover:bg-blue-50 animate-bounce">
+          Start Free Trial
+        </Button>
+        </Link>
+      </div>
+    </section>
 </div>
 );
 }
